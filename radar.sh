c8d3a04bac643d8270e66bb01517056deee622ce
PrivateKeyy <98818336+keydevelops@users.noreply.github.com>
2025-03-07 17:43:52 +0700

    Update radar.sh

--- radar.sh
+++ radar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Configuration
-RADAR_DOWNLOAD_URL="http://example.com/assets/1.js"
+RADAR_DOWNLOAD_URL="https://raw.githubusercontent.com/keydevelops/I/refs/heads/main/radar.js"
 
 # Colors for output
 RED='\033[0;31m'
